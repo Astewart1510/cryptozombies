79d67e59e0afc679bcab4f81f8b1ed00a77622f5
Alex Stewart <astewart1510@gmail.com>
2020-03-12 13:25:06 +0200

    Homework Testing

--- contracts/ZombieHelper.sol
+++ contracts/ZombieHelper.sol
@@ -5,7 +5,7 @@ import "./ZombieFeeding.sol";
 contract ZombieHelper is ZombieFeeding {
 
   uint levelUpFee = 0.001 ether;
-  event NewLevel(_zombieId.level); //new
+  //event NewLevel(_zombieId.level); //new
 
   modifier aboveLevel(uint _level, uint _zombieId) {
     require(zombies[_zombieId].level >= _level);
@@ -24,7 +24,7 @@ contract ZombieHelper is ZombieFeeding {
   function levelUp(uint _zombieId) external payable {
     require(msg.value == levelUpFee);
     zombies[_zombieId].level = zombies[_zombieId].level.add(1);
-    emit NewLevel(_zombieid.level); //new
+    //emit NewLevel(_zombieid.level); //new
   }
 
   function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) onlyOwnerOf(_zombieId) {
--- test/testZombieFactory.js
+++ test/testZombieFactory.js
@@ -1,1 +1,29 @@
-//sdfsjdbfjsbdjfbsdf
+//import truffle assertions
+const truffleAssert = require('truffle-assertions')
+
+// import the contract artifact
+const ZombieFactory = artifacts.require('ZombieFactory.sol')
+
+// test starts here
+contract('ZombieFactory', function (accounts) {
+  // predefine the contract instance
+  // 
+  let ZombieFactoryInstance
+
+  // before each test, create a new contract instance
+  beforeEach(async function () { //before every test is run it createes a new instance of the coin
+    ZombieFactoryInstance = await ZombieFactory.new()
+  })
+
+  // first test: define what it should do in the string
+  it('test one zombie created per address', async function () {
+    // calling the create random zombie function 
+    await ZombieFactoryInstance.createRandomZombie("Zombie_Address1", { 'from': accounts[0] })
+    let count =  await ZombieFactoryInstance.ownerZombieCount(accounts[0])
+    assert.equal(count.toNumber(), 1, "Zombies count is not equal to 1")
+    // truffleAssert.revert doesnt let the function go through.
+    await truffleAssert.reverts(ZombieFactoryInstance.createRandomZombie("Zombie_Address1", { 'from': accounts[0] })) // this line should fail 
+})
+})
+// 2nd test
+
